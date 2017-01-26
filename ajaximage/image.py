c385d93fe2aa4af6777534b042d15bf9732fc9e6
NikitaMorev <n.morev@melsoft-games.com>
2017-01-26 15:10:27 +0300

    revert fix for test

--- ajaximage/image.py
+++ ajaximage/image.py
@@ -47,7 +47,6 @@ def resize(file_, max_width=0, max_height=0, crop=0):
 
 
 def get_sizes(file_):
-    # with Image.open(file_) as im:
-    #     width, height = im.size
-    # return width, height
-    return 1024, 768
+    with Image.open(file_) as im:
+        width, height = im.size
+    return width, height
