dea9f4be08940f707d821c9c47f2ef365cd5d4a7
NikitaMorev <n.morev@melsoft-games.com>
2017-01-26 15:19:01 +0300

    fix closed file 2

--- ajaximage/views.py
+++ ajaximage/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.core.files.storage import default_storage
 from django.http import HttpResponse
 from django.utils.text import slugify
+from django.core.files.base import ContentFile
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from .image import resize, get_sizes
@@ -32,7 +33,7 @@ def ajaximage(request, upload_to=None, max_width=None, max_height=None, crop=Non
             return HttpResponse(data, content_type="application/json", status=403)
 
         valid_width, valid_height = int(valid_width or 0), int(valid_height or 0)
-        width, height = get_sizes(file_ .copy())
+        width, height = get_sizes(ContentFile(file_ .read()))
 
         if (valid_width and valid_width != width) or (valid_height and valid_height != height):
             data = json.dumps({'error': 'Bad image size: %sx%s is required' % (valid_width, valid_height)})
