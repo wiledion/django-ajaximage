980de73f793db936ff7ce51bbf0486cd4ec675ef
wiledion <bonzime@hotmail.com>
2017-07-15 00:27:05 +0000

    Remove 'patterns' from imports.
    Remove 'pattern' from imports. Deprecated since version 1.8 of Django

--- ajaximage/urls.py
+++ ajaximage/urls.py
@@ -1,13 +1,12 @@
 try:  # pre 1.6
     from django.conf.urls.defaults import url, patterns
 except ImportError:
-    from django.conf.urls import url, patterns
+    from django.conf.urls import url
 
-urlpatterns = patterns(
-    '',
+urlpatterns =  [
     url(
         '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)/(?P<valid_width>\d+)/(?P<valid_height>\d+)/(?P<max_bytes>\d+)',
         'ajaximage.views.ajaximage',
         name='ajaximage'
     ),
-)
+]
