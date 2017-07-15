c0403452c6c3f1ba09aff40fb346b1c1fffbe5a8
wiledion <bonzime@hotmail.com>
2017-07-15 00:40:25 +0000

    Update urls.py

--- ajaximage/urls.py
+++ ajaximage/urls.py
@@ -1,12 +1,14 @@
 try:  # pre 1.6
     from django.conf.urls.defaults import url, patterns
 except ImportError:
-    from django.conf.urls import url
+    from django.conf.urls import url  # after 1.8
+
+from .views import ajaximage
 
 urlpatterns =  [
     url(
         '^upload/(?P<upload_to>.*)/(?P<max_width>\d+)/(?P<max_height>\d+)/(?P<crop>\d+)/(?P<valid_width>\d+)/(?P<valid_height>\d+)/(?P<max_bytes>\d+)',
-        'ajaximage.views.ajaximage',
+        ajaximage,
         name='ajaximage'
     ),
 ]
